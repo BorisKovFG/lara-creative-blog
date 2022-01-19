7ff493353166fa95e564dcdbad89f763b72dd703
BorisKovFG <multiallin@gmail.com>
2022-01-19 22:46:23 +0400

    changed routing with main and add index, show of posts

---
+++ app/Http/Controllers/Post/ShowController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Carbon\Carbon;
+
+class ShowController extends Controller
+{
+    public function __invoke(Post $post)
+    {
+        $date = Carbon::parse($post->created_at);
+        $formatData = $date->translatedFormat('M d, Y • h:m a');
+        $relatedPosts = Post::where('category_id', $post->category_id)
+            ->where('id', '!=', $post->id)
+            ->inRandomOrder()
+            ->limit(3)
+            ->get();
+        return view('post.show', compact('post', 'formatData', 'relatedPosts'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController')->name('main.index');
 //    Route::get('login', 'IndexController')->name('main.index');
-
+});
+Route::group(['namespace' => 'Post', 'prefix' => 'posts'], function () {
+    Route::get('/', 'IndexController')->name('post.index');
+    Route::get('/{post}', 'ShowController')->name('post.show');
+//    Route::get('login', 'IndexController')->name('main.index');
 });
 Route::group(['namespace' => 'Personal', 'prefix' => 'personal', 'middleware' => ['auth', 'verified']], function () {
     Route::group(['namespace' => 'Main', 'prefix' => 'main'], function () {
